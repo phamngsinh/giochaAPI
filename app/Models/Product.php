de50ef3027305a0fb86c0ef4b2f6e58f9eca3dfa
smagic39 <smagic39@gmail.com>
2016-03-13 00:16:24 +0700

    modify response to validation

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,7 +10,7 @@ class Product extends Model
     protected $fillable = ['name','price','description','creator'];
 
     public function user(){
-        $this->belongsTo('App\User','creator','id');
+        $this->belongsTo('App\Models\User','creator','id');
     }
 
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,31 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Auth\Authenticatable;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends Model implements AuthenticatableContract,
-                                    AuthorizableContract,
-                                    CanResetPasswordContract
+class User extends \App\User
 {
-    use Authenticatable, Authorizable, CanResetPassword;
 
-    /**
-     * The database table used by the model.
-     *
-     * @var string
-     */
-    protected $table = 'users';
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = ['name', 'email', 'password','first_name','last_name'];
 
     /**
@@ -35,12 +20,11 @@ class User extends Model implements AuthenticatableContract,
      *
      * @var array
      */
-    protected $hidden = ['password', 'remember_token'];
 
     public function products(){
-        $this->hasMany('App\Product','creator','id');
+        $this->hasMany('App\Models\Product','creator','id');
     }
     public function orders(){
-        $this->hasMany('App\Order','user_id','id');
+        $this->hasMany('App\Models\Order','user_id','id');
     }
 }
